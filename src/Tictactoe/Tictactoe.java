3165f5a21fc16f333c6f6a348a37ac37bb08974f
manikiranp <manikiranp3@gmail.com>
2020-11-03 16:13:01 +0530

    tictactoe empty board created

--- src/Tictactoe/Tictactoe.java
+++ src/Tictactoe/Tictactoe.java
@@ -1,22 +1,20 @@
 package Tictactoe;
 
 public class Tictactoe {
-public static char[] board = new char[10];  
-
-
+static char[] board = new char[10];  
 public static void main(String args[]) {
 	System.out.println("Tictactoe Game---");
 	Board();
 }
 
 
-private static void Board() {
-	for (int i=1; i<=9; i++) {
+private static char[] Board() {
+	for (int i=1; i<board.length; i++) {
+		
 		board[i]=' ';
-	System.out.println(board[i]);
 	}
+	return board;
+		
 	
-}
-	
-	
+	}	
 }
