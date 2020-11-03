24fccf7f61b9f4811dc7aa4089f1675f438ad30f
manikiranp <manikiranp3@gmail.com>
2020-11-03 16:30:36 +0530

    determining between player and computer to plat the game

--- src/Tictactoe/Tictactoe.java
+++ src/Tictactoe/Tictactoe.java
@@ -1,10 +1,35 @@
 package Tictactoe;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Tictactoe {
 static char[] board = new char[10];  
 public static void main(String args[]) {
-	System.out.println("Tictactoe Game---");
+	System.out.println("Welcome to Tictactoe Game---");
 	Board();
+	Inputchoice();
+	
+}
+
+
+private static void Inputchoice() {
+	Scanner inp = new Scanner(System.in);
+	System.out.println("Choose 'X' OR 'O' as input: ");
+	String choice = inp.nextLine();
+	Random rand = new Random();
+	int randnum = rand.nextInt(2);
+	System.out.println("Determining between Player or computer");
+	if(randnum==1) {
+		System.out.println("Player inputs:");
+	}
+	else {
+		System.out.println("Computer inputs:");
+	}
+	
+	
+	
+	
 }
 
 
@@ -14,7 +39,6 @@ private static char[] Board() {
 		board[i]=' ';
 	}
 	return board;
-		
-	
 	}	
 }
+
