1d2544c0ba699fd40a03edcbade77a6ba9556887
manikiranp <manikiranp3@gmail.com>
2020-12-02 20:18:09 +0530

    Checks for winner or draw and prints the result

--- src/Tictactoe/Tictactoe.java
+++ src/Tictactoe/Tictactoe.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Tictactoe {
-static char[] board = new char[10];
-private static char compchoice;
-private static char playchoice;
+static String[] board = new String[10];
+private static String compchoice;
+private static String playchoice;
 private static Scanner userInput;
 static Set<Integer> indexset=new LinkedHashSet<Integer>();
 public static void main(String args[]) {
@@ -17,7 +17,9 @@ public static void main(String args[]) {
 	Tictactoe.Board();
 	Tictactoe.Inputchoice();
 	Tictactoe.ShowBoard(board);
+	
 	while(indexset.size()<=9) {
+	Tictactoe.checkWinner();
 	Tictactoe.SelectLocation(board);
 	Tictactoe.Selectloc_comp(board);
 	}
@@ -25,7 +27,7 @@ public static void main(String args[]) {
 
 
 
-private static char[] SelectLocation(char[] board) {
+private static String[] SelectLocation(String[] board2) {
 	System.out.println("Select the index to place: "+ playchoice); 
 	int cond=1;
 	while(cond==1) {
@@ -34,8 +36,8 @@ private static char[] SelectLocation(char[] board) {
 		int index = userinput.nextInt();
 		if  (((index > 0 ) && (index < 10)) && (!indexset.contains(index)) ) {
 		indexset.add(index);
-		board[index] = playchoice;
-		ShowBoard(board);
+		board2[index] = playchoice;
+		ShowBoard(board2);
 		cond=0;
 	}
 	else {
@@ -43,10 +45,10 @@ private static char[] SelectLocation(char[] board) {
 			System.out.println("Enter again");
 	}
 	}
-	return board;
+	return board2;
 }
 
-private static char[] Selectloc_comp(char[] board) {
+private static String[] Selectloc_comp(String[] board2) {
 	Random random=new Random();
 	boolean match=true;
 	while(match==true) {
@@ -57,13 +59,13 @@ private static char[] Selectloc_comp(char[] board) {
 		else {
 		System.out.println(index+" is comp chpoice");
 		indexset.add(index);
-		board[index] = compchoice;
-		 ShowBoard(board);
+		board2[index] = compchoice;
+		 ShowBoard(board2);
 		 match=false;
 		}
 		}
 	
-	return board;
+	return board2;
 	}
 	
 	
@@ -72,14 +74,14 @@ private static char[] Selectloc_comp(char[] board) {
 
 
 
-private static void ShowBoard(char[] board) {
+private static void ShowBoard(String[] board2) {
 	System.out.println("TicTacToe Board:");
 	System.out.println("_____________");
-	System.out.println("|  "+board[1]+"|  "+board[2]+"|  "+board[3]+"|");
+	System.out.println("|  "+board2[1]+"|  "+board2[2]+"|  "+board2[3]+"|");
 	System.out.println("─────────────");
-	System.out.println("|  "+board[4]+"|  "+board[5]+"|  "+board[6]+"|");
+	System.out.println("|  "+board2[4]+"|  "+board2[5]+"|  "+board2[6]+"|");
 	System.out.println("─────────────");
-	System.out.println("|  "+board[7]+"|  "+board[8]+"|  "+board[9]+"|");
+	System.out.println("|  "+board2[7]+"|  "+board2[8]+"|  "+board2[9]+"|");
 	System.out.println("‾‾‾‾‾‾‾‾‾‾‾‾‾");
 	
 }
@@ -89,13 +91,14 @@ private static void Inputchoice() {
 	userInput = new Scanner(System.in);
 	System.out.println("Choose 'X' OR 'O' as input: ");
 	
-	playchoice = userInput.next().toUpperCase().charAt(0);
+	//playchoice = userInput.next().toUpperCase();
+	playchoice=userInput.next().toUpperCase();
 	
-	if(playchoice=='X') {
-			compchoice = 'O';
+	if(playchoice.equals("X")) {
+			compchoice = "O";
 	}
-	else if(playchoice=='O') {
-			compchoice = 'X';
+	else if(playchoice.equals("O")) {
+			compchoice = "X";
 	}
 	else {
 		System.out.println("Wrong input");
@@ -106,12 +109,66 @@ private static void Inputchoice() {
 }
 
 
-private static char[] Board() {
+private static String[] Board() {
 	for (int i=1; i<board.length; i++) {
 
-		board[i]=' ';
+		board[i]=" ";
 	}
 	return board;
-	}	
+	}
+
+static void checkWinner() 
+{ 
+    for (int a = 0; a < 8; a++) { 
+        String line = null; 
+
+        switch (a) { 
+        case 0: 
+            line = board[1] + board[1] + board[3]; 
+            break; 
+        case 1: 
+            line = board[4] + board[4] + board[6]; 
+            break; 
+        case 2: 
+            line = board[7] + board[8] + board[9]; 
+            break; 
+        case 3: 
+            line = board[1] + board[4] + board[7]; 
+            break; 
+        case 4: 
+            line = board[2] + board[5] + board[8]; 
+            break; 
+        case 5: 
+            line = board[3] + board[6] + board[9]; 
+            break; 
+        case 6: 
+            line = board[1] + board[5] + board[9]; 
+            break; 
+        case 7: 
+            line = board[3] + board[5] + board[7]; 
+            break; 
+        } 
+        //For X winner
+        
+        if (line.equals("XXX")) {
+        	if(playchoice.equals("X")) { 
+        	System.out.println("Player won the game"); }
+        	else	System.out.println("Player won the game"); 
+        	System.exit(0);
+            //return "X";
+        } 
+          
+        // For O winner 
+        else if (line.equals("OOO")) {
+        	if(playchoice.equals("O")) 
+            	System.out.println("Player won the game"); 
+            	else	System.out.println("Computer won the game");
+        	System.exit(0);
+            //return "O"; 
+        }
+   
+        }
+}
+	
 }
 
