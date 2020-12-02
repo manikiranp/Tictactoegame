a06c8a61e1facbb102b5b0509694dc04cacc965e
manikiranp <manikiranp3@gmail.com>
2020-12-02 17:32:02 +0530

    added functionality of computer choice and looping till game end

--- src/Tictactoe/Tictactoe.java
+++ src/Tictactoe/Tictactoe.java
@@ -1,37 +1,74 @@
 package Tictactoe;
 
+import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Tictactoe {
 static char[] board = new char[10];
 private static char compchoice;
 private static char playchoice;
-private static Scanner userInput;  
+private static Scanner userInput;
+static Set<Integer> indexset=new LinkedHashSet<Integer>();
 public static void main(String args[]) {
 	System.out.println("---Welcome to Tictactoe Game---");
 	Tictactoe game = new Tictactoe();
 	Tictactoe.Board();
 	Tictactoe.Inputchoice();
 	Tictactoe.ShowBoard(board);
+	while(indexset.size()<=9) {
 	Tictactoe.SelectLocation(board);
-	
+	Tictactoe.Selectloc_comp(board);
+	}
 }
 
 
 
-private static char[] SelectLocation(char[] board2) {
+private static char[] SelectLocation(char[] board) {
 	System.out.println("Select the index to place: "+ playchoice); 
-	Scanner userinput = new Scanner(System.in);
-	int index = userinput.nextInt();
-		char[] board = null;
-		board2[index] = playchoice;
-		System.out.println(board2);
-		ShowBoard(board2);
-		
-	return board2;
-	
+	int cond=1;
+	while(cond==1) {
+		//System.out.println("Select the index to place: "+ playchoice);
+		Scanner userinput = new Scanner(System.in);
+		int index = userinput.nextInt();
+		if  (((index > 0 ) && (index < 10)) && (!indexset.contains(index)) ) {
+		indexset.add(index);
+		board[index] = playchoice;
+		ShowBoard(board);
+		cond=0;
+	}
+	else {
+			System.out.println("Incorrect index number");
+			System.out.println("Enter again");
+	}
+	}
+	return board;
 }
+
+private static char[] Selectloc_comp(char[] board) {
+	Random random=new Random();
+	boolean match=true;
+	while(match==true) {
+	int index=random.nextInt(9)+1;
+	
+	if(indexset.contains(index))  
+		match=true;
+		else {
+		System.out.println(index+" is comp chpoice");
+		indexset.add(index);
+		board[index] = compchoice;
+		 ShowBoard(board);
+		 match=false;
+		}
+		}
+	
+	return board;
+	}
+	
+	
+
+
 
 
 
@@ -71,7 +108,7 @@ private static void Inputchoice() {
 
 private static char[] Board() {
 	for (int i=1; i<board.length; i++) {
-		
+
 		board[i]=' ';
 	}
 	return board;
